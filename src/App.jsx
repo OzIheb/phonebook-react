5e1d81a68e2501bb2b338d124309270ea9ed704e
Iheb Ouelhazi <ouelhazi3iheb@gmail.com>
2022-09-01 16:42:14 +0100

    Add filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", num: "94030342" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
@@ -44,9 +47,23 @@ const App = () => {
     setNewNum(event.target.value);
   };
 
+  const filter = (event) => {
+    const query = event.target.value;
+    let filteredList = [...persons];
+    console.log("the list is ", filteredList);
+    filteredList = filteredList.filter((person) => {
+      return person.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    setPersons(filteredList);
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>phonebook</h2>
+      <div>
+        Filter shown with a <input type="text" onChange={filter} />
+      </div>
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={updatePersons} />{" "}
